7ef78c559d486fd22e7fef35bfba77cfba8d1491
Forrest Meade <forrest.meade@mantech.com>
2015-11-09 12:14:20 -0500

    start to Employee

--- Employee.java
+++ Employee.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 public class Employee {
 	
-	public Employee() {
-		
+	private String name;
+	private int id;
+	private String position;
+	private String supervisor; 
+	private BigInteger salary;
+	
+	public Employee(_name, _id, _position, _supervisor, _salary) {
+		this.name = _name;
+		this.id = _id;
+		this.position = _position;
+		this.supervisor = _supervisor;
+		this.salary = _salary;
+	}
+	
+	public void setSalary(BigInteger _salary) {
+		this.salary = _salary;
 	}
 }
